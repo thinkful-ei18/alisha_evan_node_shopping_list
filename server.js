6b189a100b8f949124b41d38454d52c082f434de
Evan Garrett <evang522@gmail.com>
2018-01-31 12:30:28 -0500

    finished PUT routes

--- server.js
+++ server.js
@@ -96,8 +96,62 @@ app.delete('/recipes/:id', (req, res) => {
 
 
 
+// PUT ROUTES
+
+app.put('/shopping-list/:id',(req, res) => {
+  const requiredFields = ['name', 'budget', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating shopping list item \`${req.params.id}\``);
+  ShoppingList.update({
+    id: req.params.id,
+    name: req.body.name,
+    budget: req.body.budget
+  });
+  res.status(204).end();
+});
+
+
+app.put('/recipes/:id', (req,res)=> {
+  const requiredFields = ['name','id','ingredients'];
+  requiredFields.forEach((field) => {
+    if (!(field in req.body)) {
+      const errMessage = `Missing key ${field} in request!`;
+      console.error(errMessage);
+      res.status(400).send(errMessage);
+    }
+  });
+
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  Recipes.update(req.body);
+  res.status(204).end();
+});
+
+
+
+// app.post('/testing', (req,res) => {
+//     console.log(req.body);
+// });
+
 
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
+
